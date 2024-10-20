8c9e044eddf18e1df8baa91df348515539343f32
almaldinmastara <almaldin999@gmail.com>
2024-10-21 00:55:27 +0200

    add files

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta  name="viewport" content="width=device-width,initial-scale=1">
+
+<title></title>
+
+    </head>
+
+    <body>
+
+        <h1>ADMIN</h1>
+
+    <form method="POST" action="{{ route('logout') }}">
+     <input type="submit" value="logout">
+    </form>
+
+    </body>
+</html>
+
